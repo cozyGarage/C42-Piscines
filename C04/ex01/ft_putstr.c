23601ad83071608a7bac05279d70d7cfab5ab3bc
trunnguy <helo@42vienna.com>
2024-01-27 14:57:59 +0100

    putstr

---
+++ C04/ex01/ft_putstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trunnguy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 17:05:53 by trunnguy          #+#    #+#             */
+/*   Updated: 2024/01/17 18:14:12 by trunnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+/*
+int main(void)
+{
+	char src[] = "print this thing out!";
+	ft_putstr(src);
+	return (0);
+}*/
