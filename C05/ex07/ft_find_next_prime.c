8fc783b9a5afd29e3e586e1349b9d46589771c11
Trung Nguyen <sendtotrungnguyen@outlook.com>
2024-01-27 02:56:01 +0200

    Update prime number check function to use custom square root function

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -90,7 +90,7 @@ int ft_is_prime(int nb)
     if (nb < 2)
         return 0;
     
-    for (int i = 2; i <= sqrt(nb); i++)
+    for (int i = 2; i <= ft_sqrt(nb); i++)
     {
         if (nb % i == 0)
             return 0;
