83ee93b99325f1b3e0302c454d4985a78228d6cf
trunnguy <helo@42vienna.com>
2024-01-27 14:55:02 +0100

    lowercase

---
+++ C02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trunnguy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 19:58:17 by trunnguy          #+#    #+#             */
+/*   Updated: 2024/01/14 20:00:35 by trunnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+/*#include <stdio.h>
+int	main()
+{
+	printf("%d", ft_str_is_lowercase("abcdefghijkl"));
+	printf("\n%d", ft_str_is_lowercase("abcAdefghijkl"));
+	printf("\n%d", ft_str_is_lowercase("-_134556efSghij67"));
+}*/
