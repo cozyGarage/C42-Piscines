360d4085c684ce7de420d8c4179d001dd7eb1344
Trung Nguyen <sendtotrungnguyen@outlook.com>
2024-01-27 02:51:26 +0200

    Add ft_sqrt and ft_is_prime functions to ft_find_next_prime.c

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -58,3 +58,57 @@ int		ft_find_next_prime(int nb)
 
 /* ************************************************************************** */
 /* ************************************************************************** */
+
+int ft_sqrt(int nb) {
+    if (nb < 0) {
+        return 0;
+    }
+
+    if (nb == 0 || nb == 1) {
+        return nb;
+    }
+
+    int low = 1;
+    int high = nb;
+    int result = 0;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (mid <= nb / mid) {
+            low = mid + 1;
+            result = mid;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return result;
+}
+
+int ft_is_prime(int nb)
+{
+    if (nb < 2)
+        return 0;
+    
+    for (int i = 2; i <= sqrt(nb); i++)
+    {
+        if (nb % i == 0)
+            return 0;
+    }
+    
+    return 1;
+}
+
+int ft_find_next_prime(int nb)
+{
+    if (nb < 2)
+        return 2;
+    
+    if (nb % 2 == 0)
+        nb++;
+
+    while (!ft_is_prime(nb))
+        nb += 2;
+
+    return nb;
+}
