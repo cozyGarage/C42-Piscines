109b8136df65b51d46f0de363c1a20b57185e4c8
trunnguy <helo@42vienna.com>
2024-01-27 14:55:19 +0100

    str printable

---
+++ C02/ex06/ft_str_is_printable.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trunnguy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 20:06:30 by trunnguy          #+#    #+#             */
+/*   Updated: 2024/01/14 20:09:46 by trunnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_str_is_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 32 && str [i] <= 126))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+/*#include <stdio.h>
+int	main()
+{
+	printf("%d", ft_str_is_printable("ABDELKFSCO?I340990%"));
+	printf("\n%d", ft_str_is_printable("\n\t\v\f"));
+}*/
+/*https://www.ascii-code.com/characters/printable-characters*/
