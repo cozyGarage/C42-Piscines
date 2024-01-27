abb0a09819640f5192859d9c21cc08c444a7d195
trunnguy <helo@42vienna.com>
2024-01-27 14:59:12 +0100

    print rev params

---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trunnguy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 20:12:49 by trunnguy          #+#    #+#             */
+/*   Updated: 2024/01/18 20:13:17 by trunnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		j = 0;
+		while (argv[i][j] != '\0' && argc)
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i--;
+	}
+	return (0);
+}
