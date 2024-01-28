595cc2f909f077d9459293346f7757d1b440a47e
Trung Nguyen <sendtotrungnguyen@outlook.com>
2024-01-29 00:54:10 +0200

    update C02 ex10

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -10,20 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
+	unsigned int	count;
 
 	i = 0;
-	if (src == 0 || dest == 0)
-		return (0);
-	while (src[i] != '\0' && i < size - 1)
+	count = 0;
+	while (src[count])
+		count++;
+	if (size < 1)
+		return (count);
+	while (src[i] && i < size - 1)
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (i);
+	return (count);
 }
 /*#include <stdio.h>
 int main(void)
